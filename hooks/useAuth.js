7dbac13a60b49d2be1a5b8d833df8193b0696268
Hamid <mail.hamid.hoseini@gmail.com>
2022-07-26 23:08:31 -0700

    initialize Google auth

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,12 +1,29 @@
 import { View, Text } from 'react-native'
 import React, { createContext, useContext } from 'react'
+import * as Google from 'expo-google-app-auth';
 
 const AuthContext = createContext({})
+const config = {
+  androidClientId: '',
+  iosClientId: '',
+  scopes: ["profile", "email"],
+  permissions: ["public_profile", "email", "gender", "location"],
+}
 
 export const AuthProvider = ({ children }) => {
+  const signInWithGoogle = async () => {
+    await Google.logInAsync(config).then(async (logInResult) => {
+      if (logInResult.type === "success") {
+        // login...
+      }
+    })
+  }
+
   return (
     <AuthContext.Provider value={{
-      user: "Hamid"
+      user: null,
+      signInWithGoogle
+
     }}>
       {children}
     </AuthContext.Provider>
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
 import useAuth from '../hooks/useAuth'
 
 const LoginScreen = () => {
+  const { signInWithGoogle } = useAuth();
 
   const { user } = useAuth();
   console.log(user)
   return (
     <View>
       <Text>Welcome { user }!</Text>
+      <Button title="login" onPress={signInWithGoogle} />
     </View>
   )
 }
